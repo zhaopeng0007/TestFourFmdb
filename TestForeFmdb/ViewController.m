e95d89da5c45759db4084e49de47a7891392b827
zhaopeng <wszppygr@126.com>
2016-10-18 15:27:37 +0800

    add unitTest for asset

--- TestForeFmdb/ViewController.m
+++ TestForeFmdb/ViewController.m
@@ -24,4 +24,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (int)getNum {
+    
+    return 100;
+}
+
 @end
--- TestForeFmdbTests/TestForeFmdbTests.m
+++ TestForeFmdbTests/TestForeFmdbTests.m
@@ -7,8 +7,10 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "ViewController.h"
 
 @interface TestForeFmdbTests : XCTestCase
+@property (nonatomic,strong) ViewController *vc;
 
 @end
 
@@ -17,16 +19,26 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    //初始化的代码，在测试方法调用之前调用
+    self.vc = [[ViewController alloc] init];
+
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.vc = nil;
+
     [super tearDown];
+    // 释放测试用例的资源代码，这个方法会每个测试用例执行后调用
+
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // 测试用例的例子，注意测试用例一定要test开头
+    [self testMyFuc];
+
 }
 
 - (void)testPerformanceExample {
@@ -34,6 +46,14 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (void)testMyFuc {
+    
+    // 调用需要测试的方法，
+    int result = [self.vc getNum];
+    // 如果不相等则会提示@“测试不通过”
+    XCTAssertEqual(result, 120,@"测试不通过");
 }
 
 @end
